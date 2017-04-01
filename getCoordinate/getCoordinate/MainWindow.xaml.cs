17f58e1a49deb642b14faf3ec5da62ab52da2a22
Nooheat <nooheat1228@gmail.com>
2017-04-01 20:05:56 +0900

    mouse hooking program
    When you click the mouse, the coordinates of the mouse are captured
    even if the point is outside the program.

---
+++ getCoordinate/getCoordinate/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Data;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows.Threading;
+using Gma.UserActivityMonitor;
+using System.Runtime.InteropServices;
+
+namespace getCoordinate
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // string mouseClickPosition = null;
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            var mouseHook = new MouseHook();
+            MouseHook.MouseClicked += MouseHook_MouseClicked;
+            mouseHook.Start();
+
+            Closing += delegate
+            {
+
+                mouseHook.Stop();
+            };
+            
+            
+        }
+
+        private void MouseHook_MouseClicked(object sender, MouseClickedEventArgs e)
+        {
+            var point = e.Hook.pt;
+
+            XYbox.Text = $"({point.x},{point.y})";
+        }
+    }
+}
---
+++ getCoordinate/getCoordinate/MouseHook.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace getCoordinate
+{
+    public class MouseHook
+    {
+        // Mouse Click Event Handler
+        public static event EventHandler<MouseClickedEventArgs> MouseClicked;
+
+        // Start hooking
+        public void Start() 
+        {
+            _hookID = SetHook(_proc);
+        }
+
+        // Stop hooking
+        public void Stop()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static LowLevelMouseProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static IntPtr SetHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc,
+                  GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+        
+
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+        
+
+        // use this  when your mouse move, wheel, down, up, drag, even click (all your mouse event)
+        private static IntPtr HookCallback(
+          int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            
+            // when your mouse left button down
+            if (nCode >= 0 && MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam)
+            {
+                MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+
+                // The EventHandler "MouseCliked" is invoked , MouseHook_MouseClicked(object sender, MouseClickedEventArg e) in "MainWindow.xaml"
+                MouseClicked?.Invoke(null, new MouseClickedEventArgs { Hook = hookStruct /*Property*/});
+                
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private const int WH_MOUSE_LL = 14;
+
+        private enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205
+        }
+
+
+       
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+          LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+          IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    }
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int x;
+        public int y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MSLLHOOKSTRUCT
+    {
+        public POINT pt;
+        public uint mouseData;
+        public uint flags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    public class MouseClickedEventArgs : EventArgs
+    {
+        public MSLLHOOKSTRUCT Hook { get; set; }
+    }
+}
