0e8a33f8c332e4c92bd9153efb03f97c3035fab2
Nooheat <nooheat1228@gmail.com>
2017-04-03 12:18:05 +0900

    use KeyBDHook

--- getCoordinate/getCoordinate/MainWindow.xaml.cs
+++ getCoordinate/getCoordinate/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.ComponentModel;
 using System.Threading;
 using System.Windows.Threading;
 using Gma.UserActivityMonitor;
+using System.Threading;
 using System.Runtime.InteropServices;
 
 namespace getCoordinate
@@ -27,29 +28,79 @@ namespace getCoordinate
     public partial class MainWindow : Window
     {
         // string mouseClickPosition = null;
+        List<POINT> pointList = new List<POINT>();
         
-        
+        private POINT point;
         public MainWindow()
         {
             InitializeComponent();
+
             var mouseHook = new MouseHook();
             MouseHook.MouseClicked += MouseHook_MouseClicked;
             mouseHook.Start();
 
+            var keybdHook = new KeyBdHook();
+            KeyBdHook.KeyDown += KeyBdHook_KeyDown;
+            KeyBdHook.KeyUp += KeyBdHook_KeyUp;
+            keybdHook.Start();
+
+            PointListBox.MouseLeftButtonDown += PointListBox_MouseLeftButtonDown;
+
             Closing += delegate
             {
-
                 mouseHook.Stop();
+                keybdHook.Stop();
             };
+        }
+
+        private void PointListBox_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            XYLabel.Content = PointListBox.SelectedItem;
+        }
+
+        private void KeyBdHook_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void KeyBdHook_KeyDown(object sender, KeyEventArgs e)
+        {
             
+            var key = e.Key;
+            if(key == Keys.F7)
+            {
+                if (POINT.IsAddable(new POINT { x = point.x, y = point.y }, pointList))
+                {
+                    pointList.Add(new POINT { x = point.x, y = point.y });
+                    PointListBox.Items.Add(pointList.Last());
+                    
+                }
+              
+            }
+
+            else if(key == Keys.F8)
+            {
+                POINT pt;
+                GetCursorPos(out pt);
+                if (POINT.IsAddable(pt,pointList))
+                {
+                    pointList.Add(pt);
+                    PointListBox.Items.Add(pointList.Last());
+                }
+                    XYLabel.Content = pt.ToString();
+            }
             
         }
+        
 
         private void MouseHook_MouseClicked(object sender, MouseClickedEventArgs e)
         {
-            var point = e.Hook.pt;
-
-            XYbox.Text = $"({point.x},{point.y})";
+            point = e.Hook.pt;
+            XYLabel.Content = $"({point.x},{point.y})";
         }
+
+        [DllImport("user32")]
+        public static extern Int32 GetCursorPos(out POINT pt);
+        
     }
 }
